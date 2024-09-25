5d8b1e6f82604d67ead024132efa3f7f4868a237
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-09-25 20:46:05 +0800

    wip: Search box (not functioning)

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -1,9 +1,19 @@
 <template>
-    <n-empty description="placeholder" />
+    <div style="display: flex; justify-content: center; height: 100%">
+        <n-input
+            round
+            placeholder="Search..."
+            style="position: relative; width: 75%; top: 50%"
+        >
+            <template #suffix>
+                <search fill="#888" />
+            </template>
+        </n-input>
+    </div>
 </template>
 
 <script setup>
-import { NEmpty } from "naive-ui"
+import { Search } from "@icon-park/vue-next"
 </script>
 
 <style></style>
