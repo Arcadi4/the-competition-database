5be133c1d5ef3b36792d082be034ee6b391b7226
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-10-13 12:44:46 +0800

    feat: Using JWT for authentication

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -1,7 +1,33 @@
 import express from "express";
 import { DBHandler, PendingApprovalEvent } from "../db";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const app = express();
+
+app.use(express.json());
+
+app.use(express.json());
+
+app.post("/api/admin/login", async (req, res) => {
+    const { adminKey } = req.body;
+
+    if (!process.env.ADMIN_KEY || !process.env.JWT_SECRET) {
+        return res
+            .status(500)
+            .json({ error: "Error configuration on the server side." });
+    }
+
+    const isMatch = bcrypt.compare(adminKey, process.env.ADMIN_KEY);
+    if (!isMatch) {
+        return res.status(403).json({ error: "Wrong admin key." });
+    }
+
+    const token = jwt.sign({ role: "admin" }, process.env.JWT_SECRET, {
+        expiresIn: "72h",
+    });
+    res.json({ token });
+});
 
 app.get("/", (req, res) => {
     const frontendUrl = process.env.FRONTEND_URL;
@@ -51,21 +77,37 @@ app.post("/api/event/add", (req, res) => {
     res.status(201).json({ id: eventID });
 });
 
-app.post("/api/event/approve", async (req, res) => {
-    const { adminKey, eventId } = req.body;
+app.post(
+    "/api/event/approve",
+    (req, res, next) => {
+        if (!process.env.JWT_SECRET) {
+            return res
+                .status(500)
+                .json({ error: "Error configuration on the server side." });
+        }
 
-    if (adminKey !== process.env.ADMIN_KEY) {
-        return res.status(403).json({ error: "Wrong admin key." });
-    }
-    try {
+        const authHeader = req.headers["authorization"];
+        const token = authHeader && authHeader.split(" ")[1];
+        if (token == null) return res.sendStatus(401);
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) return res.sendStatus(403);
+            if (
+                !decoded ||
+                typeof decoded !== "object" ||
+                decoded.role !== "admin"
+            )
+                return res.sendStatus(403);
+            next();
+        });
+    },
+    async (req, res) => {
+        const { eventId } = req.body;
         const result = await DBHandler.getInstance().approveEvent(eventId);
-        res.status(200).json({ message: `Event approved (id: ${result})` });
-    } catch (err) {
-        res.status(400).json({ error: `Faild to approve event (err: ${err})` });
-        return;
+        if (result) {
+            res.status(200).json({ message: `Event approved, id: ${result}` });
+        } else {
+            res.status(400).json({ error: "Failed to approve event." });
+        }
     }
-});
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+);
